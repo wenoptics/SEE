25da296dee67c7d30ed15587c68f12350a9c683a
Grayson Wen <wenoptics@163.com>
2019-01-21 17:14:47 -0500

    prototyping the ecosystem mode (connections thumbnails, network)

---
+++ frontend_prototype/js/utilities.js
@@ -0,0 +1,39 @@
+
+// Helper function to render a dom HTML string
+function render(domHtmlString, targetNode) {
+    targetNode.innerHTML = domHtmlString;
+}
+
+// Helper function to append html dom to a parent
+function appendDom(parentNode, domHtmlString) {
+    // const _wrap = document.createElement('div');
+    // render(domHtmlString, _wrap);
+    // parentNode.appendChild(_wrap);
+    parentNode.innerHTML += domHtmlString;
+}
+
+// Helper function to join path
+function pathJoin(parts, sep){
+    var separator = sep || '/';
+    var replace = new RegExp(separator+'{1,}', 'g');
+    return parts.join(separator).replace(replace, separator);
+}
+
+function ajax(method, url, ascyn, onSuccess, onFail) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            onSuccess.call(this.responseText);
+        }
+    };
+    xhttp.open(method, url, ascyn);
+    xhttp.send();
+    if (ascyn === false) {
+        return xhttp.responseText;
+    }
+}
+
+// Helper function to auto append 'px'
+function px_(s) {
+    return s + 'px';
+}
